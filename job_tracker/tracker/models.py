b3e90c18e61188d270f7bf2561129858f57e5b18
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-02-01 22:53:22 +0530

    latest commit

---
+++ job_tracker/tracker/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+
+    company_name=models.CharField(max_length=255)
+    company_website = models.URLField(blank=True)
+    company_location = models.CharField(max_length=255, blank=True)
+
+
+    def __str__(self):
+        return self.company_name
+
