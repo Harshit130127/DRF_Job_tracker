2bc8bc45555f2533664c3919cbda5746ef1e4669
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-02-02 23:12:31 +0530

    latest commit

--- job_tracker/tracker/views.py
+++ job_tracker/tracker/views.py
@@ -1,6 +1,6 @@
 from urllib import request
 from .models import Company
-from django.http import JsonResponse
+from rest_framework.response import Response
 from .serializers import CompanySerializer
 from rest_framework.decorators import api_view
 
@@ -11,12 +11,12 @@ def company(request):
 
     company=Company.objects.all()
     serializer=CompanySerializer(company, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
 
 
-
+@api_view(['GET'])
 def company_detail(request, company_id):
     company=Company.objects.get(id=company_id)
 
     serializer=CompanySerializer(company)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
